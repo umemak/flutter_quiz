7d878ec65b723ecd6278dc11f240f280cd648827
umemak <umematsu@gmail.com>
2021-09-05 13:17:00 +0900

    wip

--- lib/EntryPage.dart
+++ lib/EntryPage.dart
@@ -1,9 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-// import 'package:flutter_quiz/TopPage.dart';
 import 'package:flutter_quiz/QuestionPage.dart';
 
-class EntryPage extends StatelessWidget {
+class EntryPage extends StatefulWidget {
+  @override
+  _EntryPageState createState() => _EntryPageState();
+}
+
+class _EntryPageState extends State<EntryPage> {
+  final _codeController = TextEditingController(text: "");
+  final _nameController = TextEditingController(text: "");
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  Future<User> signInAnon() async {
+    UserCredential result = await auth.signInAnonymously();
+    return result.user!;
+  }
+
+  // _EntryPageState() {
+  //   signInAnon().then((User user) {
+  //     this.user = user;
+  //   });
+  // }
+  Future<User> user = signInAnon();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,17 +34,29 @@ class EntryPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("名前"),
-            TextField(),
+            Text(user.uid),
+            TextFormField(
+              decoration: InputDecoration(labelText: "参加コード"),
+              controller: _codeController,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "表示名"),
+              controller: _nameController,
+            ),
             const SizedBox(height: 8),
-            OutlinedButton(
+            ElevatedButton(
               onPressed: () => {
+                // 参加コード確認
+                //   NG->再入力
+                //   OK->ドキュメントID取得
+                // 参加登録 games.docID.members.userID
+                // 開始ステータス待ち
                 Navigator.of(context)
                     .push(MaterialPageRoute(builder: (context) {
                   return QuestionPage();
                 }))
               },
-              child: Text("参加する", style: TextStyle(fontSize: 40)),
+              child: Text("参加する"),
             ),
           ],
         ),
